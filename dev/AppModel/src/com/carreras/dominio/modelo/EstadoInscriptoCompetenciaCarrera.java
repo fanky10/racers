46a66ee3fb785e6ba36e978ad2931564931a829b
Facundo Scoccia <fanky10@gmail.com>
2012-08-20 16:57:58 -0300

    es inestable pero no tanto como el anterior release jaja

--- dev/AppModel/src/com/carreras/dominio/modelo/EstadoInscriptoCompetenciaCarrera.java
+++ dev/AppModel/src/com/carreras/dominio/modelo/EstadoInscriptoCompetenciaCarrera.java
@@ -10,7 +10,9 @@ package com.carreras.dominio.modelo;
  */
 public enum EstadoInscriptoCompetenciaCarrera {
     GANADOR(1, "Ganador"), PERDEDOR(2, "Perdedor"), ROTO(3, "Roto"), ESPERANDO(4, "Esperando"), CORRIENDO(5, "Corriendo"), 
-    ADELANTADO(6, "Adelantado"), GANADOR_CATEGORIA(7, "Ganador Categoria!"), NO_CORRE(8, "No Corre"),CAIDO_CATEGORIA(8, "Caido Categoria");
+    ADELANTADO(6, "Adelantado"), GANADOR_CATEGORIA(7, "Ganador Categoria!"), NO_CORRE(8, "No Corre"),CAIDO_CATEGORIA(8, "Caido Categoria"),
+    //para cuando es un numero impar de elementos
+    PROXIMA_RONDA(8, "Poxima Ronda");
     private Integer id;
     private String descripcion;
 
